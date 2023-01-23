1b661d5d42414425d08b2eed96e0507a409cf75a
NicoBelanger <nicolas.belanger@timesphere.com>
2023-01-23 15:35:45 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 echo "Execution is being started"
 echo "**************************"
+ls /opt/hostedtoolcache/Java_Temurin-Hotspot_jdk/17.0.5-8/x64
 jmeter $@
 echo "**************************"
 echo "Execution has been completed, please check the artifacts to download the results."
